c72eab6c59900bc23652279eb9c6b9f409879d5b
lakshmikant <lakshmikant2512@gmail.com>
2022-01-08 23:19:33 +0530

    update game with UI

--- main.js
+++ main.js
@@ -1,67 +1,116 @@
-function getNoOfRounds() {
-    return prompt("rounds")
-}
 
-function userSelect() {
-    return prompt("user option");
+
+const userOptions = document.querySelectorAll(".user-option");
+const userScoreTag = document.querySelector("#user-score");
+const computerScoreTag = document.querySelector("#computer-score");
+const userSelectionTag = document.querySelector("#user-selection");
+const computerSelectionTag = document.querySelector("#computer-selection")
+const userSelectionImg = document.querySelector("#user-selection-img");
+const computerSelectionImg = document.querySelector("#computer-selection-img");
+const roundWinnerTag = document.querySelector("#round-winner");
+const finalResultWrapper = document.querySelector("#final-result-wrapper");
+const finalWinnerTag = document.querySelector("#final-winner");
+const generalWrapperChild = document.querySelectorAll("#general-wrapper > *");
+const restartBtn = document.querySelector("#restart-btn");
+
+userOptions.forEach(option => {
+    option.addEventListener("click", (e) => {
+        playRound(e)
+    })
+})
+
+restartBtn.addEventListener("click", restartGame)
+
+function userSelect(e) {
+    const targetElement = e.target;
+    const dataKey = targetElement.dataset.key;
+    return dataKey;
 }
 
 function computerSelect() {
-    const options = ["s","w","g"]
-    const randomOption =  options[Math.floor(Math.random()*options.length)];
+    const options = ["snake", "water", "gun"]
+    const randomOption = options[Math.floor(Math.random() * options.length)];
     return randomOption;
 }
 
-function playRound() {
-    const userSelection = userSelect();
+function playRound(e) {
+    const userSelection = userSelect(e);
     const computerSelection = computerSelect();
-    console.log(userSelection,computerSelection)
-    let result, winner;
-    if ((userSelection == "s" && computerSelection == "w") || (userSelection == "w" && computerSelection == "s")) {
-        result = "s";
-        (userSelection == "s") ? winner = "user" : winner = "computer";
+    updateSelection(userSelection,computerSelection);
+
+    console.log(userSelection, computerSelection)
+    let  roundWinner;
+    if ((userSelection == "snake" && computerSelection == "water") || (userSelection == "water" && computerSelection == "snake")) {
+        (userSelection == "snake") ? roundWinner = "user" : roundWinner = "computer";
     }
 
-    if ((userSelection == "w" && computerSelection == "g") || (userSelection == "g" && computerSelection == "w")) {
-        result = "w";
-        (userSelection == "w") ? winner = "user" : winner = "computer";
+    if ((userSelection == "water" && computerSelection == "gun") || (userSelection == "gun" && computerSelection == "water")) {
+        (userSelection == "water") ? roundWinner = "user" : roundWinner = "computer";
     }
 
-    if ((userSelection == "s" && computerSelection == "g") || (userSelection == "g" && computerSelection == "s")) {
-        result = "g";
-        (userSelection == "g") ? winner = "user" : winner = "computer";
+    if ((userSelection == "snake" && computerSelection == "gun") || (userSelection == "gun" && computerSelection == "snake")) {
+        (userSelection == "gun") ? roundWinner = "user" : roundWinner = "computer";
     }
     if (userSelection == computerSelection) {
-        result = "d";
-        winner = null;
+        roundWinner = "no-one";
     }
-    return [result, winner];
+    addScore(roundWinner)
 }
 
-function game() {
-    const rounds = getNoOfRounds();
-    let userScore = 0, computerScore = 0, resultArray, finalWinner;
-    for (let i = 0; i < rounds; i++) {
-        resultArray = playRound()
-        if (resultArray[1] == "user") {
-            userScore++;
-        }
-        else if (resultArray[1] == "computer") {
-            computerScore++;
-        }
-        console.log(userScore, computerScore);
+function updateSelection(userSelection,computerSelection){
+    userSelectionTag.textContent = userSelection;
+    computerSelectionTag.textContent = computerSelection;
+    userSelectionImg.setAttribute('src',`./assets/${userSelection}.png`);
+    computerSelectionImg.setAttribute('src',`./assets/${computerSelection}.png`)
+}
+
+
+let userScore = 0, computerScore = 0;
+
+function addScore(roundWinner) {
+    if (roundWinner == "user") {
+        userScore++;
+        roundWinnerTag.textContent = roundWinner;
     }
-    
-    if(userScore > computerScore){
-        finalWinner = "user";
-    }
-    else if(userScore < computerScore){
-        finalWinner = "computer"
+    else if (roundWinner == "computer") {
+        computerScore++;
+        roundWinnerTag.textContent = roundWinner;
     }
     else{
-        finalWinner = "draw"
+        roundWinnerTag.textContent = roundWinner;
     }
-    console.log(finalWinner)
+
+    updateScore(userScore,computerScore);
+    checkGameProgress(userScore,computerScore)
 }
 
-game()
+function updateScore(userScore,computerScore){
+    userScoreTag.textContent=userScore;
+    computerScoreTag.textContent=computerScore;
+}
+
+function checkGameProgress(userScore,computerScore){
+    let finalWinner= null;
+    if(userScore == 5){
+        finalWinner = "user";
+    }
+    else if(computerScore == 5){
+        console.log("d");
+        finalWinner = "computer";
+    }
+    if(finalWinner!=null){
+        gameOver(finalWinner)
+    }
+}
+
+function gameOver(finalWinner){
+    generalWrapperChild.forEach(child => {
+        child.style.display="none";
+    });
+    finalResultWrapper.style.display="flex";
+    finalWinnerTag.textContent=finalWinner;
+}
+
+function restartGame(){
+    location.reload()
+}
